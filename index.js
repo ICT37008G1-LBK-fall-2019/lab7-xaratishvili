e5ee2ae4f62341e59822eb09cd63bdb63df610b7
Your Name <you@example.com>
2020-01-11 13:24:47 +0400

    lab 7

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const axios = require('axios');
+// const axios = require('axios');
+const $ = require('jQuery');
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-.then(response => response.json())
-.then(json => console.log(json))
+let greeting = () => console.log('tamuna');
+greeting();
+
+$(document.body).css("background-color", "yellow");
 
 
-axios.get('https://jsonplaceholder.typicode.com/todos/1')
-.then((response) => {
-  console.log(response.data);
-})
-.catch(function (error) {
-  console.log(error);
-});
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+// .then(response => response.json())
+// .then(json => console.log(json))
+
+
+// axios.get('https://jsonplaceholder.typicode.com/todos/1')
+// .then((response) => {
+//   console.log(response.data);
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
